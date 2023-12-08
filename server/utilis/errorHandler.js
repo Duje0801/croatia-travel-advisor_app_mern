8fdda8eedfadd1975103f93be6635164f17ecbcb
duje <dzemunik@yahoo.com>
2023-12-08 13:57:21 +0100

    error code 11000 fixed

--- server/utilis/errorHandler.js
+++ server/utilis/errorHandler.js
@@ -12,9 +12,11 @@ module.exports = (error, req, res, next) => {
     }
     return res.status(400).json({ status: `fail`, error: `${text}.` });
   } else if (error.code === 11000) {
+    let text = Object.keys(error.keyPattern);
+    text = text[0].charAt(0).toUpperCase() + text[0].slice(1);
     return res
       .status(400)
-      .json({ status: `fail`, error: `Destination name is already in use` });
+      .json({ status: `fail`, error: `${text} is already in use` });
   } else if (error.name === "JsonWebTokenError") {
     return res.status(400).json({ status: `fail`, error: `Invalid token` });
   } else if (error.name === "TokenExpiredError") {

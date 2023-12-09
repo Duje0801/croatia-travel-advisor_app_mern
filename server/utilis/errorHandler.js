c9a681daa36ad1ed41de26fd211599e06df91d45
duje <dzemunik@yahoo.com>
2023-12-09 15:24:12 +0100

    cleaned error handler, only one text string will be added to response

--- server/utilis/errorHandler.js
+++ server/utilis/errorHandler.js
@@ -1,27 +1,27 @@
 module.exports = (error, req, res, next) => {
+  let errorText = `Something went wrong`;
+
   if (error.name === "CastError") {
-    return res.status(400).json({
-      status: `fail`,
-      error: `Invalid ${error.path}: ${error.value}.`,
-    });
+    errorText = `Invalid ${error.path}: ${error.value}.`;
   } else if (error.name === "ValidationError") {
     let text = ``;
     for (let oneError of Object.values(error.errors)) {
       if (text === ``) text = text + `${oneError}`;
       else text = text + `, ${oneError}`;
     }
-    return res.status(400).json({ status: `fail`, error: `${text}.` });
+    errorText = `${text}.`;
   } else if (error.code === 11000) {
     let text = Object.keys(error.keyPattern);
     text = text[0].charAt(0).toUpperCase() + text[0].slice(1);
-    return res
-      .status(400)
-      .json({ status: `fail`, error: `${text} is already in use` });
+    errorText = `${text} is already in use`;
   } else if (error.name === "JsonWebTokenError") {
-    return res.status(400).json({ status: `fail`, error: `Invalid token` });
+    errorText = `Invalid token`;
   } else if (error.name === "TokenExpiredError") {
-    return res.status(400).json({ status: `fail`, error: `Token expiried` });
+    errorText = `Token expiried`;
   }
 
-  return res.status(400).json({ status: `fail`, error });
+  return res.status(400).json({
+    status: `fail`,
+    error: errorText,
+  });
 };

f13d9e8bfd61d1ed7f09b405b8b26d2d4851de84
duje <dzemunik@yahoo.com>
2023-12-29 11:35:08 +0100

    added show all reviews button

--- client/src/components/review/editDeleteReviewButtons.tsx
+++ client/src/components/review/editDeleteReviewButtons.tsx
@@ -1,12 +1,27 @@
+import { useNavigate, useParams } from "react-router-dom";
 import { IReview } from "../../interfaces/IReview";
+import { routes } from "../../routes/routes";
 
 export default function EditDeleteReviewButtons(props: {
   review: IReview;
   handleEditReview: (review: IReview) => void;
   handleDeleteId: (id: string) => void;
 }): JSX.Element {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const handleShowAll = () => {
+    navigate(`${routes.destination}/${props.review.destination.name}`);
+    window.location.reload();
+  };
+
   return (
     <div className="reviewButtons">
+      {params.reviewId ? (
+        <button className="reviewEditButton" onClick={() => handleShowAll()}>
+          Show all reviews
+        </button>
+      ) : null}
       <button
         className="reviewEditButton"
         onClick={() => props.handleEditReview(props.review)}
